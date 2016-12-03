c4b88a98419cfa1c8f74b62a09694f3985f3f287
arntisaksen <arnt.isaksen@gmail.com>
2016-12-03 21:10:55 +0100

    Monolake Homepage v1
    First version of the new Mmmmmonolake page.

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    blink();
+})
+
+function blink(){
+$(".animated-background")
+    .animate({backgroundColor: '#FF0000'}, 150)
+    .animate({backgroundColor: '#FF9500'}, 150)
+    .animate({backgroundColor: '#FFD200'}, 150)
+    .animate({backgroundColor: '#74E600'}, 150)
+    .animate({backgroundColor: '#1049A9'}, 150)
+    .animate({backgroundColor: '#4212AF'}, 150)
+    .animate({backgroundColor: '#A600A6'}, 150, function(){blink()});
+}
+
+function showWhite(){
+    var audio = $(".superhans")[0];
+    $(".white").fadeIn("fast");
+    audio.pause();
+}
+
+function hideWhite(){
+    var audio = $(".superhans")[0];
+    audio.addEventListener('ended', function(){this.currentTime = 0;}, false);
+    $(".white").fadeOut("fast");
+    audio.play();
+    echo("white hidden");
+}
